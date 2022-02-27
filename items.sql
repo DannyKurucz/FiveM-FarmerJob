76204b06e9670389d53af8114203b3e3e6e24a03
DannyKurucz <67187417+DannyKurucz@users.noreply.github.com>
2022-02-27 20:27:04 +0100

    Update items.sql

--- items.sql
+++ items.sql
@@ -3,3 +3,11 @@ INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
     ('milk', 'Milk', 1, 0, 1),
     ('potato', 'Potato', 1, 0, 1)
 ;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('farmer', 'Farmer')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('farmer', 0, 'farmer', 'Farmer', 0, '{}', '{}')
+;
